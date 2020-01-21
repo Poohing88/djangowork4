37f62edb8f4d3478a52ab577df88512989a20483
Mihail Shkatov <mashturma@gmail.com>
2020-01-21 17:49:54 +0300

    add corect

--- work_with_database/phones/views.py
+++ work_with_database/phones/views.py
@@ -7,31 +7,29 @@ from phones.models import Phone
 def show_catalog(request):
     template = 'catalog.html'
     sort = request.GET.get('sort')
-    information = Phone.objects.all()
     info = []
+    if sort == 'name':
+        information = Phone.objects.all().order_by('name')
+    elif sort == 'min_price':
+        information = Phone.objects.all().order_by('price')
+    else:
+        information = Phone.objects.all()
     for i in information:
         read_dick = {'Название': i.name, 'Изображение': i.image, 'Цена': i.price,
                      'Дата_выхода': i.release_date, 'Технология_lte': i.lte_exists, 'slug': i.slug}
         info.append(read_dick)
-    if sort == 'name':
-        info.sort(key=itemgetter('Название'))
-    elif sort == 'min_price':
-        info.sort(key=itemgetter('Цена'))
-    else:
-        info = info
     context = {'info': info}
     return render(request, template, context)
 
 
 def show_product(request, slug):
     template = 'product.html'
-    information = Phone.objects.all()
+    information = Phone.objects.all().filter(slug=slug)
     info = {}
     for i in information:
         read_dick = {'Название': i.name, 'Изображение': i.image, 'Цена': i.price,
                      'Дата_выхода': i.release_date, 'Технология_lte': i.lte_exists, 'slug': i.slug}
-        if read_dick['slug'] == slug:
-            info = read_dick
+        info = read_dick
     context = {"info": info}
     return render(request, template, context)
 
